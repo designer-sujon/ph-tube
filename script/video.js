1e2d182e72c056246bec612182b4ace0416f9d51
designer-sujon <cdnl2018@gmail.com>
2024-10-03 11:48:41 +0600

    completed

--- script/video.js
+++ script/video.js
@@ -25,9 +25,9 @@ const loadCatagories = () => {
 
 }
 
-const loadVideos = () => {
+const loadVideos = (searchText="") => {
     // fetch
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then(res=> res.json())
     .then(data => displayVideos(data.videos))
     .catch(error => console.log(error))
@@ -46,6 +46,24 @@ const loadCategoriesVideos= (id) =>{
     })
     .catch(error => console.log(error))
 }
+const videoDetails = async (videoID) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phero-tube/video/${videoID}`)
+    const data = await res.json();
+    displayDetails(data.video)
+
+}
+
+const displayDetails =(video)=>{
+    console.log(video)
+    const detailContainer = document.getElementById('modal-content')
+    detailContainer.innerHTML = `
+    <img src=${video.thumbnail} />
+    <p>${video.description}</p>
+    `
+    // document.getElementById("showModalData").click();
+    document.getElementById("customModal").showModal();
+}
+
 // const demo = {
 //     "category_id": "1001",
 //     "video_id": "aaag",
@@ -102,7 +120,7 @@ const displayVideos= (videos)=>{
         <p class="text-gray-400">${video.authors[0].profile_name}</p>
         ${video.authors[0].verified === true ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />`: ''}
         </div>
-        <p> </p>
+        <p> <button onclick="videoDetails('${video.video_id}')" class="btn btn-sm btn-error">Details</button></p>
         </div>
     </div>
   </div>
@@ -126,5 +144,9 @@ const displayCatagories = (categories) =>{
 
 }
 
+document.getElementById('search-input').addEventListener("keyup", (e)=>{
+    loadVideos(e.target.value)
+
+})
 loadCatagories()
 loadVideos()
